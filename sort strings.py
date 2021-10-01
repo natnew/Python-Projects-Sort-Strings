786e89a4607423ee3a738572f3cbcd4878ac6307
natnew <natashanewboldlondon@gmail.com>
2021-10-01 11:12:37 +0100

    Add files via upload

---
+++ sort strings.py
@@ -0,0 +1,15 @@
+print()
+message = 'Welcome to my humble abode!'
+number = '192746'
+print(message)
+print(number)
+
+message_sorter = sorted(message)
+number_sorted = sorted(number)
+
+print(message_sorter)
+print(number_sorted)
+
+print('Sort strings by words:')
+message_sorter_word = sorted(message.split())
+print(message_sorter_word)
